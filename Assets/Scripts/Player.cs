342c233fe853ae680cb01e43a919d0d5a7dc1745
pikoscielniak <piotrek.koscielniak@gmail.com>
2022-10-03 08:39:18 +0200

    added bounderies

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] float speed = 2;
+
+    private Rigidbody _rigidbody;
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        _rigidbody.AddForce(horizontal * speed, 0, vertical * speed, ForceMode.VelocityChange);
     }
 }
